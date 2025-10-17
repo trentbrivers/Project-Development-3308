e2ff6e43d65552d766bd0b3ff7cfc143a012bb73
Roger Barrett <roger.barrettjr@gmail.com>
2025-10-16 23:14:28 -0700

    Added Initialization Route + Minor Additions to GameState

--- src/backend/core.py
+++ src/backend/core.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from dataclasses import dataclass
 from enum import Enum
 
@@ -27,20 +27,37 @@ class GameState:
     timer: int                   # if timer > 0, we can still answer questions, otherwise time is up
     category: str
     question: str
+    question_value: int
     answer: str
     answer_status: AnswerStatus  # see AnswerStatus class for definition
     game_status: GameStatus      # see GameStatus class for definition
+    current_score: int            # current player score
     game_id: int                 # this is the ID tracking which jeopardy! game we're referencing
 
 
-    def __init__(self, timer, category, question, answer, answer_status, game_status, game_id):
+    def __init__(self, timer, category, question, question_value, answer, answer_status, game_status, current_score, game_id):
         self.timer = timer
         self.category = category
         self.question = question
+        self.question_value = question_value
         self.answer = answer
         self.answer_status = answer_status
         self.game_status = game_status
+        self.current_score = current_score
         self.game_id = game_id
+
+#To do: Migrate question specific attributes from GameState to Question?
+@dataclass
+class Question:
+    question: str
+    question_value: int
+    answer: str
+    answer_status: AnswerStatus
+    def __init__(self, question, question_value, answer, answer_status):
+        self.question = question
+        self.question_value = question_value
+        self.answer = answer
+        self.answer_status = answer_status
 
 
 @app.route('/submit_answer', methods=['POST'])
@@ -76,10 +93,37 @@ def submit_answer():
         timer=0 if correct else game_state.timer,
         category=game_state.category,
         question=game_state.question,
+        question_value=game_state.question_value,
         answer=game_state.answer,
         answer_status=AnswerStatus.CORRECT.value if correct else AnswerStatus.INCORRECT.value,
         game_status=GameStatus.IN_PROGRESS.value,
+        current_score=game_state.current_score, #plus if correct, minus if incorrect will need to add
         game_id=6692
     )
 
     return jsonify(new_game_state)
+
+#Initial function called to populate data on the frontend
+#Generates the game state for internal tracking and pushes
+#required information to frontend
+@app.route('/initialize_game', methods=['GET'])
+def initialize_game():
+    ### On initialize call to database to generate 30 questions (6 categories of 5 questions)  ###
+
+    ### will need to loop through and select questions ###
+
+    # Dummy placeholders for now
+    start_game_state = GameState(
+        timer=1000,  #don't need to use for now
+        category='finance',
+        question='in value investing this is the most commonly used metric for finding intrinsic value',
+        question_value=1000,
+        answer='discounted cash flow',
+        answer_status=AnswerStatus.UNANSWERED.value,
+        game_status=GameStatus.IN_PROGRESS.value,
+        current_score=0,
+        game_id=6692
+    )
+
+    return jsonify(start_game_state)
+
