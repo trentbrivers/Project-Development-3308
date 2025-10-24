b09b481040676afd52367a9f418c27f76c1ea5c8
eazar001 <ebrahim.azarisooreh@gmail.com>
2025-10-24 16:54:55 -0600

    Updating game-state with intermediate data structure for RJ.

--- src/backend/core.py
+++ src/backend/core.py
@@ -24,23 +24,21 @@ class GameStatus(Enum):
 # mock game state, we can add more or delete as needed
 @dataclass
 class GameState:
-    timer: int                   # if timer > 0, we can still answer questions, otherwise time is up
+    timer: int                    # if timer > 0, we can still answer questions, otherwise time is up
     category: str
-    question: str
-    question_value: int
-    answer: str
-    answer_status: AnswerStatus  # see AnswerStatus class for definition
-    game_status: GameStatus      # see GameStatus class for definition
+    questions: list[str]
+    answers: list[str]
+    answer_status: AnswerStatus   # see AnswerStatus class for definition
+    game_status: GameStatus       # see GameStatus class for definition
     current_score: int            # current player score
-    game_id: int                 # this is the ID tracking which jeopardy! game we're referencing
+    game_id: int                  # this is the ID tracking which !jeopardy game we're referencing
 
 
-    def __init__(self, timer, category, question, question_value, answer, answer_status, game_status, current_score, game_id):
+    def __init__(self, timer, category, questions, answers, answer_status, game_status, current_score, game_id):
         self.timer = timer
         self.category = category
-        self.question = question
-        self.question_value = question_value
-        self.answer = answer
+        self.question = questions
+        self.answer = answers
         self.answer_status = answer_status
         self.game_status = game_status
         self.current_score = current_score
@@ -89,17 +87,11 @@ def submit_answer():
     actual_answer = categories[game_state.category][game_state.question]
     correct = user_answer.replace(' ', '').lower() == actual_answer.replace(' ', '').lower()
 
-    new_game_state = GameState(
-        timer=0 if correct else game_state.timer,
-        category=game_state.category,
-        question=game_state.question,
-        question_value=game_state.question_value,
-        answer=game_state.answer,
-        answer_status=AnswerStatus.CORRECT.value if correct else AnswerStatus.INCORRECT.value,
-        game_status=GameStatus.IN_PROGRESS.value,
-        current_score=game_state.current_score, #plus if correct, minus if incorrect will need to add
-        game_id=6692
-    )
+    new_game_state = GameState(timer=0 if correct else game_state.timer, category=game_state.category,
+                               questions=[], answers=[],
+                               answer_status=AnswerStatus.CORRECT.value if correct else AnswerStatus.INCORRECT.value,
+                               game_status=GameStatus.IN_PROGRESS.value, current_score=game_state.current_score,
+                               game_id=6692)
 
     return jsonify(new_game_state)
 
@@ -112,26 +104,34 @@ def initialize_game():
 
     ### will need to loop through and select questions ###
 
+    mock_questions = [['q0', 'q1', 'q2', 'q3', 'q4'],
+                      ['q5', 'q6', 'q7', 'q8', 'q9'],
+                      ['q10', 'q11', 'q12', 'q13', 'q14'],
+                      ['q15', 'q16', 'q17', 'q18', 'q19'],
+                      ['q20', 'q21', 'q22', 'q23', 'q24'],
+                      ['q25', 'q26', 'q27', 'q28', 'q29']]
+
+    mock_answers = [['a0', 'a1', 'a2', 'a3', 'a4'],
+                    ['a5', 'a6', 'a7', 'a8', 'a9'],
+                    ['a10', 'a11', 'a12', 'a13', 'a14'],
+                    ['a15', 'a16', 'a17', 'a18', 'a19'],
+                    ['a20', 'a21', 'a22', 'a23', 'a24'],
+                    ['a25', 'a26', 'a27', 'a28', 'a29']]
+
     # Dummy placeholders for now
-    start_game_state = GameState(
-        timer=1000,  #don't need to use for now
-        category='finance',
-        question='in value investing this is the most commonly used metric for finding intrinsic value',
-        question_value=1000,
-        answer='discounted cash flow',
-        answer_status=AnswerStatus.UNANSWERED.value,
-        game_status=GameStatus.IN_PROGRESS.value,
-        current_score=0,
-        game_id=6692
-    )
+    start_game_state = GameState(timer=1000, category='finance',
+                                 questions=mock_questions,
+                                 answers=mock_answers,
+                                 answer_status=AnswerStatus.UNANSWERED.value,
+                                 game_status=GameStatus.IN_PROGRESS.value,
+                                 current_score=0,
+                                 game_id=6692)
 
     return jsonify(start_game_state)
 
 
 # just adding an indexer as code documentation for frontend's board-array indexing scheme
-def get_idx(cat_idx, clue_idx):
+def get_idx(row_idx, cat_idx):
     # we have six categories and five clues, so a 5 x 6 board
-    m, n = (5, 6)
-    assert clue_idx < m and cat_idx < n
-
-    return cat_idx * (n - 1) + clue_idx
+    limit_idx = 6
+    return row_idx * limit_idx + cat_idx
