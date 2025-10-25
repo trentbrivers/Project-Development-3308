cb91fdafd46d368dd9e884b947bb4c961a4449e3
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2025-10-25 10:43:24 -0600

    Merging in Database Layer (#1)
    * rame4494 basic DDL done
    
    * rame4494 feature branch small dir restructure
    
    * rame4494 DB logic & pathing fixes
    
    * rame4494 Questions all constraint logic working
    
    * rame4494 Game6692 in db
    
    * Functionize Extract Game + Driver Code for Extracting Questions/Answers
    
    ---------
    
    Co-authored-by: RAMertz0630 <mertz.rachel.a@gmail.com>
    Co-authored-by: Roger Barrett <roger.barrettjr@gmail.com>

---
+++ src/db/dbDDL.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import sqlite3
+
+dbPath = Path(__file__).parent.resolve().joinpath('notJeopardyDB.db')
+con = sqlite3.connect(dbPath)
+cur = con.cursor()
+
+# Cleanup actions - start w/ blank slate
+cur.executescript("""
+    BEGIN;
+    DROP TABLE IF EXISTS Contestant;
+    DROP TABLE IF EXISTS PlayerAnswer;
+    DROP TABLE IF EXISTS GameQuestion;
+    DROP TABLE IF EXISTS Player;
+    DROP TABLE IF EXISTS Question;
+    DROP TABLE IF EXISTS Game;
+    COMMIT;
+    """)
+
+# Create tables and constraints de novo
+cur.executescript("""
+    BEGIN;
+    CREATE TABLE IF NOT EXISTS Game(GameID INTEGER PRIMARY KEY,
+                                    DisplayName VARCHAR(50) NOT NULL,
+                                    StartDate DATETIME NOT NULL,
+                                    EndDate DATETIME NOT NULL,
+                                    IsCompleteGame CHAR(1),
+                                    IsCanceledGame CHAR(1)
+                                    );
+    
+    CREATE TABLE IF NOT EXISTS Player(PlayerID INTEGER PRIMARY KEY,
+                                      UserName VARCHAR(50) NOT NULL,
+                                      TotalGamesPlayed INT NOT NULL,
+                                      TotalGamesWon INT NOT NULL,
+                                      TotalGamesRunnerUp INT NOT NULL,
+                                      HighScore INT NOT NULL,
+                                      CONSTRAINT UniqueName UNIQUE (Username)
+                                      );
+                  
+    CREATE TABLE IF NOT EXISTS Question(QuestionID INTEGER PRIMARY KEY,
+                                        Category VARCHAR(50) NOT NULL,
+                                        Round VARCHAR(2) NOT NULL,
+                                        PointValue INT NOT NULL,
+                                        QuestionText VARCHAR(500) NOT NULL,
+                                        QuestionAns VARCHAR(500) NOT NULL,
+                                        CONSTRAINT UniqueQ UNIQUE (QuestionText)
+                                        );
+                  
+    CREATE TABLE IF NOT EXISTS Contestant(GameID INT,
+                                          PlayerID INT,
+                                          PlayerScore INT NOT NULL,
+                                          CONSTRAINT GameContestFK FOREIGN KEY (GameID) REFERENCES Game (GameID)
+                                              ON DELETE CASCADE
+                                              ON UPDATE CASCADE,
+                                          CONSTRAINT PlayerConstestFK FOREIGN KEY (PlayerID) REFERENCES Player (PlayerID)
+                                              ON DELETE CASCADE
+                                              ON UPDATE CASCADE
+                                          );
+    
+    CREATE TABLE IF NOT EXISTS GameQuestion(GameID INT,
+                                            QuestionID INT,
+                                            IsAnswered CHAR(1) NOT NULL,
+                                            CONSTRAINT GameLogFK FOREIGN KEY (GameID) REFERENCES Game (GameID)
+                                                ON DELETE CASCADE
+                                                ON UPDATE CASCADE,
+                                            CONSTRAINT QuestionBankFK FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
+                                                ON DELETE CASCADE
+                                                ON UPDATE CASCADE
+                                            );
+    
+    CREATE TABLE IF NOT EXISTS PlayerAnswer(PlayerID INT,
+                                           GameID INT,
+                                           QuestionID INT,
+                                           AnswerText VARCHAR(500) NOT NULL,
+                                           IsCorrect CHAR(1) NOT NULL,
+                                           CONSTRAINT AnsweringPlayerFK FOREIGN KEY (PlayerID) REFERENCES Player (PlayerID)
+                                               ON DELETE CASCADE
+                                               ON UPDATE CASCADE,
+                                           CONSTRAINT AnswerGameFK FOREIGN KEY (GameID) REFERENCES GameQuestion (GameID)
+                                               ON DELETE CASCADE
+                                               ON UPDATE CASCADE,
+                                            CONSTRAINT AnsweredQFK FOREIGN KEY (QuestionID) REFERENCES GameQuestion (QuestionID)
+                                               ON DELETE CASCADE
+                                               ON UPDATE CASCADE
+                                            );
+    COMMIT;
+    """)
+
+con.commit()
+con.close()
---
+++ src/db/dbQC.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+import sqlite3
+
+filePath = Path(__file__).parent.resolve()
+
+def print_tables(dbPath:Path, dbFilename:str):
+    
+    dbPath = dbPath.joinpath(dbFilename) 
+    con = sqlite3.connect(dbPath)
+    cur = con.cursor()
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+    print ("\nTables:")
+    for t in cur.fetchall() :
+        print ("\t[%s]"%t[0])
+
+     ##   print ("\tColumns of", t[0])
+        cur.execute("PRAGMA table_info(%s);"%t[0])
+        for attr in cur.fetchall() :
+            print ("\t\t", attr)
+        
+        print ("")
+
+def print_rows(dbPath:Path, dbFilename:str, tblnames:list):
+    """Helper function to check that test data inserted into
+    the database displays correctly."""
+    
+    dbPath = dbPath.joinpath(dbFilename) 
+    con = sqlite3.connect(dbPath) 
+    cur = con.cursor()
+
+    for tbl in tblnames:
+        print(f'\n{tbl}:')
+        res = cur.execute(f"SELECT * FROM {tbl};")
+        print(res.fetchall())
+
+    con.close()
+
+def testQuestion(dbPath:Path, dbFilename:str):
+    """Prototype tests for table Question"""
+    
+    dbPath = dbPath.joinpath(dbFilename) 
+    con = sqlite3.connect(dbPath) 
+    cur = con.cursor()
+
+    # Positive Control: This should work & show rowid aliasing in action
+    PosCtrl1 = [('Testing', 100, 'Why are we doing this?', 'To test basic RI actions.'),
+               ('Birds', 500, 'This unusual shorebird has showy females and drab males.', "Wilson's Pharalope")]
+    
+    cur.executemany("INSERT INTO Question (Category, PointValue, QuestionText, QuestionAns) VALUES(?, ?, ?, ?)", PosCtrl1)
+    con.commit()
+
+    # This should also work - different questions can have the same answer
+    PosCtrl2 = ('Testing', 200, 'Why do we validate table constraints?', 'To test basic RI actions.')
+    cur.execute("INSERT INTO Question (Category, PointValue, QuestionText, QuestionAns) VALUES(?, ?, ?, ?)", PosCtrl2)
+    con.commit()
+
+    # This should fail - violates uniqueness constraint
+    NegCtrl1 = ('Testing', 0, 'Why do we validate table constraints?', 'Broken DB logic is no fun.')
+    cur.execute("INSERT INTO Question (Category, PointValue, QuestionText, QuestionAns) VALUES(?, ?, ?, ?)", NegCtrl1)
+    con.commit()
+
+    con.close()
+
+def cleanupHelper(dbPath:Path, dbFilename:str, tbls:list=[]):
+    """Truncates the tables passed in parameter list.
+    If the default empty list is passed, all tables are
+    truncated."""
+
+    dbPath = dbPath.joinpath(dbFilename) 
+    con = sqlite3.connect(dbPath) 
+    cur = con.cursor()
+
+    if tbls == []:
+        cur.executescript("""
+                          BEGIN;
+                          DELETE FROM Contestant;
+                          DELETE FROM PlayerAnswer;
+                          DELETE FROM GameQuestion;
+                          DELETE FROM Player;
+                          DELETE FROM Question;
+                          DELETE FROM Game;
+                          COMMIT;
+                          """)
+    
+    else:
+        for tbl in tbls:
+            cur.execute(f"DELETE FROM {tbl};")
+    
+    con.commit()
+    con.close()
+
+# QC after DDL
+print_tables(filePath, 'notJeopardyDB.db')
+
+# DML and constraints QC here
+testQuestion(filePath, 'notJeopardyDB.db')
+print_rows(filePath, 'notJeopardyDB.db', ['Question'])
+cleanupHelper(filePath, 'notJeopardyDB.db')
+print_rows(filePath, 'notJeopardyDB.db', ['Question'])
